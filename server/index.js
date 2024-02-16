4ad063c093d087655b1906c1982b4537150e66d1
AdrianeDriane <dilaoaparre@gmail.com>
2024-02-16 12:02:27 +0800

    feat: post method for saving new book

--- server/index.js
+++ server/index.js
@@ -1,10 +1,37 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  return response.status(200).send('Welcome to Book Store');
+});
+
+app.post('/books', async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: 'Please send all required fields: title, author, publishYear',
+      });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ messasge: err.message });
+  }
+});
 
 mongoose
   .connect(mongoDBURL)
