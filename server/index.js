2c97e1f77885c32b8c9d68959878bcf46ea1a5e7
AdrianeDriane <dilaoaparre@gmail.com>
2024-02-17 17:13:48 +0800

    feat: show all books

--- server/index.js
+++ server/index.js
@@ -10,14 +10,14 @@ const app = express();
 // for Parsing Request Body with JSON
 app.use(express.json());
 // for Handling CORS Policy
-// app.use(cors());
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get('/', (req, res) => {
   return response.status(200).send('Welcome to Book Store');
