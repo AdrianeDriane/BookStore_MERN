de80b63d09966a3992f33d3965c6b2e8ac63614b
AdrianeDriane <dilaoaparre@gmail.com>
2024-02-17 11:26:16 +0800

    fix: handled CORS policy

--- server/index.js
+++ server/index.js
@@ -2,9 +2,22 @@ import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
+
+// MIDDLEWARES
+// for Parsing Request Body with JSON
 app.use(express.json());
+// for Handling CORS Policy
+// app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 app.get('/', (req, res) => {
   return response.status(200).send('Welcome to Book Store');
